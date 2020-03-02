adb68336e186377c3d56df14e5bf11cde9419ce9
SimonJdV <50994970+SimonJdV@users.noreply.github.com>
2020-03-02 13:46:40 +0100

    Update Portal.js

--- src/portal/src/Portal.js
+++ src/portal/src/Portal.js
@@ -1,38 +1,97 @@
-import { Component } from 'react'
+/* eslint-disable unicorn/prefer-query-selector, no-eq-null, eqeqeq */
+import React, { Component } from 'react'
 import canUseDom from 'dom-helpers/util/inDOM'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
-let portalContainer
+const _id = 'evergreen-portal-container'
 
-export default class Portal extends Component {
-  constructor() {
-    super()
+function createPortalContainer() {
+  const portalContainer = document.createElement('div')
+  portalContainer.id = _id
+  portalContainer.setAttribute(_id, '')
+
+  return portalContainer
+}
+
+class Portal extends Component {
+  constructor(props) {
+    super(props)
 
     // This fixes SSR
     if (!canUseDom) return
 
-    if (!portalContainer) {
-      portalContainer = document.createElement('div')
-      portalContainer.setAttribute('evergreen-portal-container', '')
-      document.body.appendChild(portalContainer)
+    if (this.props.contextId) {
+      this.portalContainer = this.props.contextWindow.document.getElementById(
+        _id
+      )
+
+      if (this.portalContainer == null) {
+        this.portalContainer = createPortalContainer()
+        this.props.contextWindow.document.body.append(this.portalContainer)
+      }
+    } else if (!this.props.contextHasPopup) {
+      this.portalContainer = document.getElementById(_id)
+
+      if (this.portalContainer == null) {
+        this.portalContainer = createPortalContainer()
+        document.body.append(this.portalContainer)
+      }
     }
 
-    this.el = document.createElement('div')
-    portalContainer.appendChild(this.el)
+    if (this.portalContainer != null) {
+      this.el = document.createElement('div')
+      this.portalContainer.append(this.el)
+    }
   }
 
   componentWillUnmount() {
-    portalContainer.removeChild(this.el)
+    if (this.portalContainer != null) {
+      this.portalContainer.removeChild(this.el)
+    }
   }
 
   render() {
     // This fixes SSR
-    if (!canUseDom) return null
+    if (!canUseDom || this.portalContainer == null) return null
     return ReactDOM.createPortal(this.props.children, this.el)
   }
 }
 
 Portal.propTypes = {
+  children: PropTypes.node.isRequired,
+  contextHasPopup: PropTypes.bool.isRequired,
+  contextId: PropTypes.string,
+  contextWindow: PropTypes.object
+}
+
+export const HasPopupContext = React.createContext(false)
+
+export const WidgetContext = React.createContext({
+  id: null,
+  window: null
+})
+
+PortalWithContext.propTypes = {
   children: PropTypes.node.isRequired
+}
+
+export default function PortalWithContext(props) {
+  return (
+    <HasPopupContext.Consumer>
+      {hasPopup => (
+        <WidgetContext.Consumer>
+          {context => (
+            <Portal
+              contextHasPopup={hasPopup}
+              contextId={context.id}
+              contextWindow={context.window}
+            >
+              {props.children}
+            </Portal>
+          )}
+        </WidgetContext.Consumer>
+      )}
+    </HasPopupContext.Consumer>
+  )
 }
